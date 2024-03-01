1a88290b9f896fdd3a29b1219b3e903f4daf74d6
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-03-01 09:01:52 +0000

    Add files via upload

---
+++ Files/01_comments.py
@@ -0,0 +1,9 @@
+# comments in Python
+
+# single line comments
+
+# multiple comments , select the lines of code and press enter + forward slash.
+# a = 1
+# b = 3
+# c = 25
+# name = input('What is  your name: ')
