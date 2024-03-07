389bd4dd90e6b96f24e7c26c5f64859099ecb413
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-03-07 18:40:19 +0000

    Add files via upload
    Working more on numbers in Python

---
+++ Files/05_numbers-2.py
@@ -0,0 +1,18 @@
+# arithmetics shorthands:  += , -= , /= , *=
+calories = 50
+#calories += 200
+#calories -= 20
+#calories *=8
+calories /=5
+
+# floor (round to nearest whole number)
+print(19//5)
+
+# exponential method
+print(8**2)
+
+# string cohesion (integer to float, etc)
+print(int(2.99876)) # this will chop off the numbers after the decimal point
+print(float(20)) # this will add .0 to the number
+
+
