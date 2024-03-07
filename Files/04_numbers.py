d678facbf8451c58996554d5db628983960616ab
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-03-07 01:04:34 +0000

    Add files via upload
    Working with numbers in Python

---
+++ Files/04_numbers.py
@@ -0,0 +1,18 @@
+# Numbers in python : integers and floats
+
+# integers
+ages = 30
+print(type(ages))
+
+# float ( decimals)
+soccer_rating = 9.5
+print(type(soccer_rating))
+
+# Arithmetics in Python
+py = 5 + 2 * 5 / (45 - 35)
+
+birth_date = 1985
+current_date = 2024
+age = current_date - birth_date
+
+print(age)
