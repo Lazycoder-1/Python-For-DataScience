d803fcd025c923616441fa1d75d144619609c1fe
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-03-08 09:20:53 +0000

    Add files via upload
    Working with strings in Python

---
+++ Files/07_strings.py
@@ -0,0 +1,17 @@
+# strings
+
+name_1 = 'ohene'
+name_2 = "ohene"
+name_3 = '''ohene'''
+# print(name_1)
+# print(name_2)
+# print(name_3)
+
+# other scenario
+print("I'm going home early because of the rain")
+
+# when quoting someone's speech
+print('Mohammed Ali said "fly like a butterfly and sting like a bee".')
+
+# using the backslah key(\) , place it like this 'they\'re eg.
+print('They\'re over there shouting "Fight","Fight"')
