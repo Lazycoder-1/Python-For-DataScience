ba29ed9eb43b66f2aae84c78f50df9c206988f81
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-03-09 02:17:48 +0000

    Add files via upload

---
+++ Files/08_string-indexing.py
@@ -0,0 +1,18 @@
+# Indexing of string 
+# length of a string
+# Empty spaces count as characters in Python
+
+word = "fhjkdsmfhskjzm"
+words = "hello"
+#print(len(word))
+
+# find specific character
+#print(word[21])
+
+# getting the characters backwards
+#print(word[-4])
+
+# String slicing [0:4]
+#print(word[0:5])
+print(word[:4]) #alternate way
+print(word[4:])
