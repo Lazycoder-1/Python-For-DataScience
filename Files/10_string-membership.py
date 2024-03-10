22668983fcf741e515fe1792219902b894cfcd62
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-03-10 12:17:01 +0000

    Add files via upload
    This script describes string membership in Python etc.

---
+++ Files/10_string-membership.py
@@ -0,0 +1,24 @@
+# string membership in Python
+# simply checks if the character exists in both, e.g 
+
+
+# when the character exists
+a = 'a' in 'Hamburger'
+cf = 'c and f' in 'chenford'
+
+
+# when the character does not exist
+b = 'e' not in 'Chicken'
+c = 'eel' not in 'food'
+
+# other scenario
+raffle_ticket = '158A'
+winner = '158a'
+
+out_come = winner in raffle_ticket
+
+#print(b)
+print(c)
+print(cf)
+print(out_come)
+
