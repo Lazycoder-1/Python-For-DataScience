736acdc5c7357b0989f3a939afbee4645a430c3c
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-03-17 10:16:45 +0000

    Add files via upload

---
+++ Files/12_format-method.py
@@ -0,0 +1,9 @@
+# using the .format method (.format()) , acting like a placeholder
+
+movie = 'The Rookie'
+fav = 'Chenford' 
+
+#print('I like watching {}, beacuse I like {} a lot'.format(movie,fav)) 
+
+# other scenario
+print('I am currently in {} at a town called {} for the {}'.format('Mankessim','Abonko','Lithium Project'))
