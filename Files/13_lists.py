3fd51a2bca6dc1407d9eea4e2148f118474085d9
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-03-18 06:51:48 +0000

    Add files via upload

---
+++ Files/13_lists.py
@@ -0,0 +1,18 @@
+# lists in Python
+
+letters = ['a','b','c','d','e','f','g','h']
+numbers = [1,2,3,4,5,6,7,8,9]
+
+# nested lists
+mixed_items = ['b',14,'o','c','d',10]
+mixed_items2 = ['b',14,[1,2,4,8,5,3],'lovlace','newton',4,5,['hello','code','repeat'],90]
+
+# simple tictactoe game structure
+tic_tac_toe = [
+    [[" "],[" "],[" "]],
+    [[" "],[" "],[" "]],
+    [[" "],[" "],[" "]]
+ ]
+
+print(mixed_items2)
+
