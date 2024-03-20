ee39ed8cf8bb75f072437b3f8a3fb8629c9a4246
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-03-20 05:45:42 +0000

    Add files via upload

---
+++ Files/14_lists-indexing.py
@@ -0,0 +1,12 @@
+# lists indexing : accessing lists items
+
+letters = ['a','b','c','d','e','f','g','h']
+numbers = [1,2,3,4,5,6,7,8,9]
+
+# another scenario, list within list 
+mixed_items2 = [14,2,8,5,[1,2,4,8,5,3],4,5,90]
+
+# using the negative index, this will give you the last item in the list
+letters = ['a','b','c','d','e','f','g','h']
+print(letters[-1])
+
