988dedb3dc56c06bce9efcb2c2fc4ee93d9b42f1
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-03-22 21:49:06 +0000

    Add files via upload

---
+++ Files/16_lists-methods.py
@@ -0,0 +1,12 @@
+# various lists methods e.g sorting, change list, 
+
+rookies = ['Grey','Tim','Lopez','Harper','Chen','Nolan','Selina','Aaron','Bailey','Randy']
+#rookies.sort()
+rookies.sort(reverse=True)   # reverse the list items in descending order
+
+# using the sorted method on variables
+fav = sorted(rookies)
+
+# changing list
+print(fav)
+
