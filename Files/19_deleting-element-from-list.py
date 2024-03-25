4eb294860149fd12865291f454fd34cb65195437
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-03-25 20:14:54 +0000

    Add files via upload

---
+++ Files/19_deleting-element-from-list.py
@@ -0,0 +1,14 @@
+# deleting element from list using the del method
+letters = ['a','b','c','d','e','f','g','h','i','j']
+#del letters[1]
+
+# using the slice method
+#del letters[1:4]
+
+# deleting all the elements in the list using the clear method
+# returns an empty list as []
+letters.clear()
+
+print(letters)
+
+
