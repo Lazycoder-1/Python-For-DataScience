d9f5fa12af68e40907c07136e2a18464278b9b54
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-03-26 20:54:40 +0000

    Add files via upload

---
+++ Files/20_other-lists-methods.py
@@ -0,0 +1,9 @@
+# other useful lists methods
+letters = ['a','b','c','d','e','f','g','h','i','j']
+
+# the length of a list
+#print(len(letters)) 
+
+# other scenario
+li = ['a','b','c','d',['e','f','g'],'h','i',['j']]
+print(li)
