5ea6c6216b6c04a42616bd45193092754ce5c026
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-03-29 08:57:33 +0000

    Add files via upload

---
+++ Files/21_lists-membership.py
@@ -0,0 +1,6 @@
+# checking whether a string is in list
+rappers = ['B.I.G','2Pac','Nas','Eminem','Rakim','DMX']
+# using the in method
+print('Eminem' in rappers)
+# using the not in method
+print('B.I.G' not in rappers)
