9bdc7219270d9ea8088952604bf38b7be1a4ee9e
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-03-31 20:12:02 +0000

    Add files via upload

--- Files/22_split-and-join-methods.py
+++ Files/22_split-and-join-methods.py
@@ -1,3 +1,8 @@
 # using the split and join methods
-# convert a string to list e.g
-print(list('hello'))
+# convert a string to list, remember to pass list(hello) e.g. ['h', 'e', 'l', 'l', 'o']
+#print(list('hello'))
+
+# split method , this will append elements to a list
+colors = 'red,green,blue'
+new_colors = colors.split(',')
+print(new_colors)
