ced3439d28c8a7a7f174de7acd8a0b800002a8c1
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-04-04 18:30:50 +0000

    Add files via upload

---
+++ Files/25_accessing-elements-from-dictionary.py
@@ -0,0 +1,9 @@
+
+# access associate element in a dictionary
+names_ages = {'Tim':35,'Lucy':29,'Nolan':43,'Bailey':36}
+#print(names_ages['Bailey'])
+
+# using the .get method
+print(names_ages.get('Tim'))
+
+# more examples
