089e5c83350cfe16218b954c974172af12c18cd3
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-04-05 06:05:02 +0000

    Add files via upload

---
+++ Files/26_change-n-add-dictionary-elements.py
@@ -0,0 +1,9 @@
+# changing and adding dictionary elements
+# change value in a dictionary 
+teams = {'Team 1':16,'Team 2':20,'Team 3':24,'Team 4':19}
+teams['Team 1'] = 20
+
+# adding values to the dictionary
+teams = {'Team 1':16,'Team 2':20,'Team 3':24,'Team 4':19}
+teams['Team 5'] = 30
+print(teams)
