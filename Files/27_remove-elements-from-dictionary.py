2f5ddd6fd5cd68e0c0b37f80352a38b83d01115e
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-04-06 05:58:47 +0000

    Add files via upload

---
+++ Files/27_remove-elements-from-dictionary.py
@@ -0,0 +1,10 @@
+# removing elements from dictionary
+# using the .pop method
+teams = {'Team 1':16,'Team 2':20,'Team 3':24,'Team 4':19}
+#print(teams.pop('Team 2')) # removes the corresponding value in the dictionary
+
+# remove all items from the dictionary
+print(teams.clear())
+
+# using the del keyword
+team = teams.del(['Team 1']) 
