377a35b9e495d4d018ed794a694670d8dc05e580
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-04-08 15:29:13 +0000

    Add files via upload

---
+++ Files/reverse-lists.py
@@ -0,0 +1,15 @@
+# simple lists analysis
+
+# reverse lists
+list1 = [100, 200, 300, 400, 500]
+#list_reverse = list1.reverse()
+
+# alt method
+list1 = list1[::-1]
+print(list1)
+
+
+
+
+# Expected output
+# [500, 400, 300, 200, 100]
