48e99afe4db5339c558546dbb311226c3883cf3c
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-04-10 16:25:36 +0000

    Add files via upload

---
+++ Files/30_introduction-to-tuples.py
@@ -0,0 +1,14 @@
+# introduction to tuples in Python
+# Tuples represented by ()
+my_tuple = (1,2,3,4,5)
+
+# tuples with different data types
+my_tuple_1 = (1,'a',3.42145963,'bc',45)
+my_tuple_nested = (1,[1,3,'food',['soccer','baseball']],'movies',345)
+
+# Tuple packing : without using the ()
+items = 1, 'Boston',[2,3,4,'New England'],62.46,76
+
+# creating a single element as a tuple - bring a comma as shown below
+item = ('winowa',)
+print(type(item))
