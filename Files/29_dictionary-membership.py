c5d7a146fb2b9a2720d43a0d413018b2b98a2191
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-04-10 05:57:03 +0000

    Add files via upload

---
+++ Files/29_dictionary-membership.py
@@ -0,0 +1,11 @@
+# dictionary membership 
+# whether a member exist in the dictionary
+shopping_items = {'ps5':2500,'macbook':4000,'console':1000,'sneakers':500}
+print('ps5'in shopping_items)
+print('ps3'not in shopping_items)
+
+# using the len method
+print(len(shopping_items))
+
+print(sorted(shopping_items))
+
