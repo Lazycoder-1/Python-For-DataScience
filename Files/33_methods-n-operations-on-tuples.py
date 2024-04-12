19f7bcaf9a62f128217303e9e12a3226bb974011
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-04-12 18:51:23 +0000

    Add files via upload

---
+++ Files/33_methods-n-operations-on-tuples.py
@@ -0,0 +1,10 @@
+# methods and operations on tuples
+
+# using the count method - the no of times a value is present in a tuple
+# Example - print(numbers.count(1)) , returns 6 since 1 occurs 6 times 
+numbers = (1,1,1,1,2,3,4,5,1,1)
+#print(numbers.count(1))
+
+# tuple membership 
+print(1 in numbers)
+print(17 in numbers)
