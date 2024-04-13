819c09963b32b86afd6d3a4b5ef46ad6cd73b4e9
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-04-13 18:19:18 +0000

    Add files via upload

---
+++ Files/34_tuple-unpacking.py
@@ -0,0 +1,8 @@
+# Tuples unpacking
+my_lists = 1,2,'codewars',3.4
+
+# tuples unpacking - reassigning of values to variables
+a,b,c,d = my_lists
+# expected results    # 1 2 codewars 3.4
+print(a,b,c,d)
+
