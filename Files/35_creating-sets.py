04622f1d0c16e473328db8879eb9a4dcbe90fed0
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-04-14 08:14:43 +0000

    Add files via upload

---
+++ Files/35_creating-sets.py
@@ -0,0 +1,11 @@
+# Sets are represented by {} 
+# sets removes duplicate values 
+
+numbers = {1,2,3,4,5}
+
+# convert a list to set using the set() method
+numbers_2 = set([1,2,3,3,3,5])
+
+# creating emppty set
+numbers_3 = set()
+print(type(numbers_3))  
