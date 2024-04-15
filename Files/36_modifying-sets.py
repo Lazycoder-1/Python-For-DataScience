72498843706d678008e99789550d7a0303a19e99
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-04-15 06:23:49 +0000

    Add files via upload

---
+++ Files/36_modifying-sets.py
@@ -0,0 +1,13 @@
+# modifying sets
+
+numbers = {1,2,3,4}
+
+# accessing individual items in a set
+#print(numbers[1]) this will throw some error when ypu run it.
+
+# add a single item to a set using the .add() method
+#numbers.add(5)
+
+# using the .update() method to add multiple elements to a set
+numbers.update([5,6,7,8])
+print(numbers)  
