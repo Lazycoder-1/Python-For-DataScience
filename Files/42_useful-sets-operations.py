18998cdfd5d24208d8432e43ea3b1d851e261776
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-04-18 05:54:01 +0000

    Add files via upload

---
+++ Files/42_useful-sets-operations.py
@@ -0,0 +1,15 @@
+# other useful methods on sets
+
+numbers = {1,2,3,4,5,6,7,8,9}
+# len method - length of the set
+#numbers = len(numbers)
+
+# max method
+#numbers_max = max(numbers)
+
+# min method 
+#numbers_min = min(numbers)
+
+# sum method 
+numbers_sum = sum(numbers)
+print(numbers_sum)  
