d4c47819acb6fd8a88fcc223e9ac352313c4df98
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-04-19 05:57:48 +0000

    Add files via upload

---
+++ Files/43_Comparison-Operators.py
@@ -0,0 +1,17 @@
+# comparison operators in control flow 
+
+# Equal operator (==) 
+num_1 = 10 == 10
+num_2 = 10 == '10'
+
+# Not equal to operator (!=)
+#a = 20 != 20
+a = 'ten' != 10
+
+# greater and less than operators = <,>
+#b = 4 < 6
+b = 5 > 2
+
+
+
+print(b)    
