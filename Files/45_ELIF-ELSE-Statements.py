9408e11770eaab1afe5971c6ff93f9ab1781bb3d
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-04-21 19:58:12 +0000

    Add files via upload

--- Files/45_ELIF-ELSE-Statements.py
+++ Files/45_ELIF-ELSE-Statements.py
@@ -1,8 +1,32 @@
 # ELIF and ELSE statements
 
-age = 14
+# age = 14
 
-if age < 18:
-    print('Dont gamble please')
-else:
-    print('Gamble wisely')
+# if age < 18:
+#     print('Dont gamble please')
+# else:
+#     print('Gamble wisely')
+
+
+# Using elif statements 
+# score = 59.999
+
+# if score >= 70:
+#     print('First Class')
+# elif score >= 60:
+#     print('Second Class Upper')
+# elif score >= 50:
+#     print('Second Class Lower')
+# else:
+#     print('Passed')
+
+# shopping list 
+shopping_list = []
+item = 'Apple Juice'
+# using the condition statement 
+if item not in shopping_list:
+    shopping_list.append(item)
+    print(shopping_list)  
+
+
+
