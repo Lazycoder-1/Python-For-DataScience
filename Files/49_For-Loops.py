6933d06f1e228015d57f56f411e3df6d6666ea23
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-04-28 10:30:06 +0000

    Add files via upload

---
+++ Files/49_For-Loops.py
@@ -0,0 +1,35 @@
+# Introduction to for loops
+
+# simple example with lists
+#for element in [1,2,3,4,5,6]:
+#    print(element)
+
+# adding all items in a list using for loops
+# numbers = [1,2,3,4,5,6,7,8,9,10]
+# total = 0
+
+# for num in numbers:
+#     total += num   
+# print(total)
+
+# using the range method - add all numbers in a range method
+# total_items = 0
+# for num in range(1,11):
+#     total_items += num
+# print(total_items)
+
+# using for loops to count the no of letters in a string
+# count the number of 'o' in the sentence
+count = 0
+msg = "I love Python"
+for letter in msg:
+    if letter == 'o':
+        count += 1
+print(count)
+
+ 
+ 
+
+
+
+    
