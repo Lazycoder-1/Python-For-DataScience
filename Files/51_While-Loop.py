b76d3c094d39db4bdec1b08a8c8596513781b4ae
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-04-30 05:47:29 +0000

    Add files via upload

---
+++ Files/51_While-Loop.py
@@ -0,0 +1,16 @@
+
+
+# i = 1
+# total = 0
+# while i <= 10:
+#     total += i
+#     i += 1
+# print(total) 
+
+# on a list 
+items = ['soccer','movies','music','games']
+i = 0
+while i < len(items):
+    print(items[i])
+    i += 1 
+
