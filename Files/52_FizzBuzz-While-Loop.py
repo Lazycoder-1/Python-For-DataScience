5b51eadd41fb813ae50f43572f106b2d61d8ecd8
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-05-01 09:18:39 +0000

    Add files via upload

---
+++ Files/52_FizzBuzz-While-Loop.py
@@ -0,0 +1,17 @@
+# FizzBuzz Game with While Loop
+
+i = 1
+stop = 17
+while i<= stop: 
+    if i % 5== 0 and i % 3 == 0:
+        print('FizzBuzz')
+    elif i % 3 == 0:
+        print('Fizz')
+    elif i % 5 == 0:
+        print('Buzz')
+    else:
+        print(i)
+    i += 1
+
+
+
