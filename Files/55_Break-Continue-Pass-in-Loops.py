3d6b1939c436ec86f36d74faa70ad935c4ccebcd
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-05-05 20:15:32 +0000

    Add files via upload

--- Files/55_Break-Continue-Pass-in-Loops.py
+++ Files/55_Break-Continue-Pass-in-Loops.py
@@ -8,10 +8,16 @@
 # print('This is the results')
 
 # continue statement
-for num in [1,2,3,4,5,6,7,8,9,10]:
-    if num == 4:
-        continue
-    print(num)
-print('This is the results after the loop')
+# for num in [1,2,3,4,5,6,7,8,9,10]:
+#     if num == 4:
+#         continue
+#     print(num)
+# print('This is the results after the loop')
+
+# the pass method - if you dont want anything to happen.....
+for i in [1,2,3,4,5]:
+    pass
+
+
 
 
