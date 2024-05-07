f3ea2d8ec28a2056ca330b0f55566b4bb58c7bf5
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-05-07 05:41:18 +0000

    Add files via upload

---
+++ Files/58_Enumerate-Function.py
@@ -0,0 +1,10 @@
+ # using the enumerate function
+alphabets = "abcdefghijkm"
+index = 0
+# for letter in alphabets:
+#      print('The letter {} is index {}'.format(letter, index))
+#      index += 1
+
+ # using the enumerate function - return a tuple structure
+for letter in enumerate(alphabets):
+    print(letter)
