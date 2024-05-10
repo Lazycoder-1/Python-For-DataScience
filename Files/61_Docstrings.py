cc9ef0c8131a611cdf77afe31d7409f0eb1b6bca
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-05-10 15:37:07 +0000

    Add files via upload

---
+++ Files/61_Docstrings.py
@@ -0,0 +1,7 @@
+# docstring = comments in functions or documentation
+def rookie():
+    '''The best drama series ever made'''
+    print('The Rookie')
+rookie()
+
+print(rookie.__doc__) # to get the documentation attached to the code 
