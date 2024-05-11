a8a7c745a786a94884aea9d1048bc9241dcc93d6
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-05-11 21:41:13 +0000

    Add files via upload

---
+++ Files/62_The-Return-Statement.py
@@ -0,0 +1,25 @@
+# using the return method/function
+
+# def rookie():
+#     '''The best drama series ever made'''
+#     message = "Tim and Lucy"
+#     return message
+# rookie().upper()
+
+# this function return sqaure numbers
+# def double(num):
+#     return num ** 2
+# print(double(8))
+
+# def my_function(x):
+#   return 5 * x
+# print(my_function(3))
+
+def play_game(answer):
+    if answer == 'yes':
+        return True
+    else:
+        return False
+print(play_game('exacy'))
+
+
