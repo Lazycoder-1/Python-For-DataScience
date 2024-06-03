2ec54834b0f86a7ce779c057bafd55bfa4c780fd
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-06-03 17:15:15 +0000

    Add files via upload

---
+++ Files/71_Lambda-Functions.py
@@ -0,0 +1,13 @@
+
+# Lambda Functions = functions defined without using the def keyword , start with lambda instead
+# They are called anonymous functions because that don’t have a name like a normal function
+
+# with only one parameter
+# double = lambda num: num * 2
+# print(double(5))
+
+# with 2 or more parameters
+multi = lambda a,b,c: (c-a)/b
+print(multi(10,4,8))
+
+
