e4eed0fcaf148e1721296b7e73d79b90e7c07f5a
Lazycoder-1 <116932740+Lazycoder-1@users.noreply.github.com>
2024-06-09 17:55:19 +0000

    Add files via upload

---
+++ Files/76_User-Inputs.py
@@ -0,0 +1,12 @@
+
+# accepting user input(s)
+# words = input('Enter an word(s): ')
+# length = len(words)
+# print('The length of your words was '+ str(length) +' long')
+
+# using it with functions
+def characters():
+    words = input('Enter an word(s): ')
+    length = len(words)
+    print('The length of your words was ' + str(length) + ' long')
+characters()
